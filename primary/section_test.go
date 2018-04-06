304233ea5d5594f057a960e0af5bfcfbebc256ba
Clare <chu.leilei@vpclub.cn>
2018-04-06 20:44:15 +0800

    slice  切片添加

--- primary/section_test.go
+++ primary/section_test.go
@@ -6,4 +6,22 @@ func TestStatementSection(t  *testing.T)  {
 	statementSection()
 }
 
+func TestTransmit(t *testing.T)  {
+	transmit()
+	Transmit1()
+}
 
+func TestTransmit1(t *testing.T)  {
+	Transmit3()
+}
+
+func TestTransmit2(t *testing.T)  {
+	transmit4()
+}
+
+func TestTransmit3(t *testing.T)  {
+
+	forSlice();
+
+
+}
